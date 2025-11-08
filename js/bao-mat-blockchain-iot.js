cffdbbfa5b44c1640b99906248f0f78a389193df
hhdoremoneleven <hhdoremoneleven@gmail.com>
2025-11-08 12:50:04 +0000

    Changes on KHCN tab

---
+++ js/bao-mat-blockchain-iot.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Load Header
+    fetch('../../components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        });
+
+    // Load Footer
+    fetch('../../components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        });
+});
